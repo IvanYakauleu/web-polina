1d00ceafb0a83b68034e49a90d3fcb99e7930956
Ivan <voodoolar333@gmail.com>
2023-09-20 15:55:22 +0200

    add review

--- src/components/review/review.tsx
+++ src/components/review/review.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image"
+import './review.scss'
+
+import client1 from '../../../public/client1.jpg'
 
 interface IReviewProps {
     photo: string;
@@ -9,7 +12,7 @@ interface IReviewProps {
 export default function Review(props: IReviewProps) {
     return (
         <div className="review">
-            <Image className="review__photo" src='../../../public/client1.jpg' width={100} height={100} alt="client photo"/>
+            <Image className="review__photo" src={props.photo} width={100} height={100} alt="client photo"/>
             <div className="review__wrapper">
                 <div className="review__name">{props.name}</div>
                 <div className="review__descr">{props.descr}</div>
