61698fc1ccf199a60c23fbbe6b576f12b08fee41
Ivan <voodoolar333@gmail.com>
2023-09-21 20:31:14 +0200

    change objectFit in review

--- src/components/review/review.tsx
+++ src/components/review/review.tsx
@@ -12,7 +12,7 @@ interface IReviewProps {
 export default function Review(props: IReviewProps) {
     return (
         <div className="review">
-            <Image className="review__photo" src={props.photo} width={100} height={100} alt="client photo"/>
+            <Image className="review__photo" src={props.photo} width={100} height={100} alt="client photo" style={{objectFit: "cover"}}/>
             <div className="review__wrapper">
                 <div className="review__name">{props.name}</div>
                 <div className="review__descr">{props.descr}</div>
