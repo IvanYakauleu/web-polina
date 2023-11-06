5185221de89dc53701310c8e68811d76bcb8814a
Ivan Yakauleu <voodoolar333@gmail.com>
2023-11-06 12:55:42 +0100

    add fullscreen photo onClick

--- src/components/photo/photo.tsx
+++ src/components/photo/photo.tsx
@@ -1,4 +1,10 @@
+'use client';
+
+import './photo.scss';
+
 import Image from 'next/image';
+
+import { useState } from 'react';
 import { StaticImageData } from 'next/image';
 
 interface IPhotoProps {
@@ -6,7 +12,33 @@ interface IPhotoProps {
 }
 
 const Photo = ({ url }: IPhotoProps) => {
-  return <Image src={url} width={268} height={380} alt="portfolio-photo" />;
+  const [selectedImage, setSelectedImage] = useState<StaticImageData | null>(null);
+
+  const openImage = (image: StaticImageData) => {
+    setSelectedImage(image);
+    document.body.style.overflow = 'hidden';
+  };
+
+  const closeImage = () => {
+    setSelectedImage(null);
+    document.body.style.overflow = 'auto';
+  };
+  return (
+    <>
+      <Image
+        src={url}
+        width={268}
+        height={380}
+        alt="portfolio-photo"
+        onClick={() => openImage(url)}
+      />
+      {selectedImage && (
+        <div className="fullscreen-image" onClick={closeImage}>
+          <Image src={url} alt="Selected Image" />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Photo;
